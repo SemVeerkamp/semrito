8cc6762896e4c75e11d13bf551b861f993ca045a
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-23 17:47:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from website import create_app
 import os
 
-PORT=4000
-os.environ.get('PORT', 5000)
+port = int(os.environ.get('PORT'))
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port)
 
