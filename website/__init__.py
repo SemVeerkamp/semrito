9382f29a2ab1f413d23d90a628eafd0211168818
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-24 16:27:59 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -9,8 +9,8 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.environ.get('DATABASE_URL')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SECRET_KEY')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     db.init_app(app)
 
     from .views import views
