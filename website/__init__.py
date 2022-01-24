4a8d95b2dc4361c7356ee75d9654ad96f5040c60
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-24 17:22:34 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -22,7 +22,7 @@ def create_app():
 
     from .models import User, Prediction
 
-    create_database(app)
+    create_database()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
