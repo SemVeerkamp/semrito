ab70dddbd56b3a825d06246fbf8a95d99cf46dc5
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-24 17:10:53 +0100

    Update models.py

--- website/models.py
+++ website/models.py
@@ -3,12 +3,14 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(150))
     name = db.Column(db.String(150))
     predictions = db.relationship('Prediction')
 
 class Prediction(db.Model):
+    __tablename__ = 'prediction'
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     event = db.Column(db.String(1000))
