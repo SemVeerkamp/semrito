32853e51e7d24bbc7034d8fccc30651b45465b2d
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-24 16:56:35 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = postgresql://otcbwpjphfhakz:6901b06fa72dac4dcaea9fa63e865d4cc707cd76eacf98f22f8efdcbf082809e@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/dehvn3qtqra7cv
     db.init_app(app)
 
     from .views import views
